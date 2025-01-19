926d4b2c6375b33452b77ce5e13f60ef644f0542
miniuwrz <dominik.zblewski.gda@gmail.com>
2025-01-19 16:30:29 +0100

    fixedstuff

--- app/components/ToolsSection.tsx
+++ app/components/ToolsSection.tsx
@@ -1,57 +1,7 @@
 import React from "react";
-import { RiNextjsFill } from "react-icons/ri";
-import { FaReact } from "react-icons/fa";
-import {
-  SiTypescript,
-  SiJavascript,
-  SiTailwindcss,
-  SiHtml5,
-} from "react-icons/si";
 
 const TechSection = () => {
-  return (
-    <div className="py-2">
-      {/* <h1 className="font-bold">Tools I use:</h1>
-      <ul className="flex flex-wrap max-608:justify-between sm:justify-start md:justify-between gap-4 mt-6 text-6xl">
-        <li
-          className="flex items-center justify-center border-[1px] w-32 h-32 border-borderclr p-4 rounded-lg hovertrans"
-          title="Next.js"
-        >
-          <RiNextjsFill />
-        </li>
-        <li
-          className="flex items-center justify-center border-[1px] w-32 h-32 border-borderclr p-4 rounded-lg hovertrans hover:text-[#61dbfb]"
-          title="React"
-        >
-          <FaReact />
-        </li>
-        <li
-          className="flex items-center justify-center border-[1px] w-32 h-32 border-borderclr p-4 rounded-lg hovertrans hover:text-[#f0db4f]"
-          title="JavaScript"
-        >
-          <SiJavascript />
-        </li>
-        <li
-          className="flex items-center justify-center border-[1px] w-32 h-32 border-borderclr p-4 rounded-lg hovertrans hover:text-[#3178c6]"
-          title="TypeScript"
-        >
-          <SiTypescript />
-        </li>
-        <li
-          className="flex items-center justify-center border-[1px] w-32 h-32 border-borderclr p-4 rounded-lg hovertrans hover:text-cyan-400"
-          title="Tailwind CSS"
-        >
-          <SiTailwindcss />
-        </li>
-        <li
-          className="flex items-center justify-center border-[1px] w-32 h-32 border-borderclr p-4 rounded-lg hovertrans hover:text-[#E44D26]"
-          title="HTML5"
-        >
-          <SiHtml5 />
-        </li>
-      </ul> */}
-    </div>
-  );
+  return <div className="py-2"></div>;
 };
 
 export default TechSection;
